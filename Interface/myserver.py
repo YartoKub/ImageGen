99930440e6c6a86f105f0985622a5e4d00b28247
JleHuBbluKoT <45314340+YartoKub@users.noreply.github.com>
2025-05-20 13:53:27 +0300

    Пора в уник

--- Interface/myserver.py
+++ Interface/myserver.py
@@ -1,16 +1,41 @@
 import socket
+HEADER200 = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+HEADER404 = 'HTTP/1.1 404 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+PAGE_FOLDER = "ImageGen\Interface\Pages"
+my_adress = ('127.0.0.1', 2000)
+def start_server():
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(my_adress)
+        server.listen(4)
+        while True:
+            #print("launched")
+            client_socket, adress = server.accept()
+            data = client_socket.recv(1024).decode('utf-8')
+            print(data)
+            request_handler(data, adress)
+            content = load_page_from_request(data)
+            client_socket.send(content)
+            client_socket.shutdown(socket.SHUT_WR)
+            print('shutddown')
+    except KeyboardInterrupt:
+        server.close()
+        print("Server down due to keyboard interrupt")
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('127.0.0.1', 2000))
-server.listen(4)
-print("launched")
-client_socket, adress = server.accept()
-data = client_socket.recv(1024).decode('utf-8')
-print(data)
+def request_handler(request_data, adress): # Эта штука должна определять тип реквеста и вызывать соответствующую функцию наверное из словаря функций?
+    print(request_data)
+    print(adress)
+def load_page_from_request(request_data):
+    path = PAGE_FOLDER + request_data.split(" ")[1] 
+    response = ''
+    try:
+        with open(path, "rb") as file:
+            response = file.read()
+        return HEADER200.encode('utf-8') + response
+    except:
+        return HEADER404.encode('utf-8') + 'This page does not exist'.encode('utf-8')
 
-content = "Response given".encode('utf-8')
-header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'.encode('utf-8')
-client_socket.send(header+content)
 
-print('shutddown')
+    
 
+start_server()
